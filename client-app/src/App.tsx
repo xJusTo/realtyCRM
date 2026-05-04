1600655ed868d102e8e0657b7c4c15452e471ded
xJusTo <2513846@mail.ru>
2026-05-04 21:03:35 +0300

    Инициализирован React проект и создан компонент списка недвижимости

---
+++ client-app/src/App.tsx
@@ -0,0 +1,11 @@
+import PropertyList from './components/PropertyList'
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', backgroundColor: '#f9fafb' }}>
+      <PropertyList />
+    </div>
+  )
+}
+
+export default App
